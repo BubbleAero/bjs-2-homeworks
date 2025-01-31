767d5aef578465ba9fcced67f33cde346d47c7c9
NetoTest <vikavasilenko2013@gmail.com>
2025-01-31 22:36:35 +0300

    Home work is done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,63 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = []; 
+
+    function wrapper(...args) {
+        const hash = md5(args); 
+        let objectInCache = cache.find((item) => item.hash === hash); 
+
+        if (objectInCache) {
+            console.log("Из кеша: " + objectInCache.value);
+            return "Из кеша: " + objectInCache.value;
+        }
+
+        let result = func(...args); 
+        cache.push({ hash, value: result }); 
+
+        if (cache.length > 5) {
+            cache.shift(); 
+        }
+
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+
+    return wrapper;
 }
+
+        
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+    let timeoutID; 
+    let isFirstCall = true; 
+
+    function wrapper(...args) {
+        wrapper.allCount++; 
+
   
+        if (isFirstCall) {
+            console.log("Первый вызов:", args);
+            func.apply(this, args); 
+            wrapper.count++; 
+            isFirstCall = false; 
+            return;
+        }
+
+        if (timeoutID) {
+            clearTimeout(timeoutID);
+        }
+
+        timeoutID = setTimeout(() => {
+            console.log("Таймаут сработал:", args);
+            func.apply(this, args); 
+            wrapper.count++; 
+        }, delay);
+    }
+
+    wrapper.count = 0; 
+    wrapper.allCount = 0; 
+
+    return wrapper;
 }
+
